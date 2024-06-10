0e3937033bfe390a4d39e356eeb2c9aa87d753e5
Atal09 <113996359+Atal09@users.noreply.github.com>
2024-06-10 15:04:14 +0200

    scroll function

--- pages/supermarkets.js
+++ pages/supermarkets.js
@@ -1,28 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 function Supermarkets({ route }) {
     const { data } = route.params;
 
     return (
         <View style={styles.container}>
-            {data && data.items && data.items.map((item, index) => (
-                <View key={index} style={styles.supermarketContainer}>
-                    <Text style={styles.supermarketText}>{item.title}</Text>
-                </View>
-            ))}
+            <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                {data && data.items && data.items.map((item, index) => (
+                    <View key={index} style={styles.supermarketContainer}>
+                        <Text style={styles.supermarketText}>{item.title}</Text>
+                    </View>
+                ))}
+            </ScrollView>
         </View>
     );
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 10,
     },
+    scrollViewContent: {
+        paddingBottom: 20,
+    },
     supermarketContainer: {
         padding: 10,
-        borderBottomWidth: 1,
+        borderBottomWidth: 3,
         borderBottomColor: '#add8e6',
     },
     supermarketText: {
